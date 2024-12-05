6ee0accc04b20dc37abf41f7a98fcee3baf963b6
pedrotokar <pedrotokar2004@gmail.com>
2024-12-04 21:53:53 -0300

    Add bus functions to header file

--- include/genBus.h
+++ include/genBus.h
@@ -4,5 +4,9 @@
 #include "graph.h"
 
 void genBusPoints(CityGraph&, int, int[]);
+void genBusLinesFull(CityGraph&, int[], float**, int**);
+float genBusLines(int, int[], float**);
+void swapPath(int[], int, int);
+float optimizeBusLines(int, int[], float**, float);
 
 #endif
