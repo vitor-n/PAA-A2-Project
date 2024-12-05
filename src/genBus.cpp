f6b507632ae7548611d09eaad321830e0c3b2463
pedrotokar <pedrotokar2004@gmail.com>
2024-12-04 21:25:01 -0300

    Finish bus line generation and improve organization

--- src/genBus.cpp
+++ src/genBus.cpp
@@ -35,108 +35,122 @@ void genBusPoints(CityGraph& cityGraph, int region, int points[]) {
     cout << "Bus Station " << region << ": " << bestNode << endl;
     points[region] = bestNode;
 }
-/*
-Graph* genSubwayLines(CityGraph& city, Graph& subwayFull, int stations[], int **path, bool bPrint){
-    int cost[city.numNodes()];
 
-    for (int i = 0; i < city.numRegions(); i++) {
-        city.CPTDijkstra(stations[i], path[i], cost, &compareCost);
-        for (int j = 0; j < city.numRegions(); j++) {
-            if (i == j) continue; 
-
-            EdgeNode* edge = new EdgeNode;
-            edge->lenght = cost[stations[j]];
-
-            if (bPrint) cout << i << " " << j << " " << edge->lenght << endl;
-
-            subwayFull.addSegment(i, j, edge);
-        }
-    }
-
-    Graph* subwayMST = subwayFull.genMSTPrim();
-    return subwayMST;
-}
-
-
-*/
-int main(){
-    int INF = 2147483647;
-    CityGraph city = cityParser("data/city-1");
-    Graph busFull = Graph(city.numRegions(), 0);
-
-    //GERAÇÃO BUS FULL
-    int points[city.numRegions()];
-    int** path = new int*[city.numRegions()];
-    for (int i = 0; i < city.numRegions(); i++){
-        path[i] = new int[city.numNodes()];
-    }
-
-
-    float distMatrix[city.numRegions()][city.numRegions()];
-    for(int i = 0; i < city.numRegions(); i++){
-        for(int j = 0; j < city.numRegions(); j++){
-            distMatrix[i][j] = -1;
-        }
-    }
-
-    for (int i = 0; i < city.numRegions(); i++) {
-        genBusPoints(city, i, points);
-    }
-
+void genBusLinesFull(CityGraph& city, int points[], float** distMatrix, int** path){
     int cost[city.numNodes()];
 
     for (int i = 0; i < city.numRegions(); i++) {
         city.CPTDijkstra(points[i], path[i], cost, &compareBuildings);
         for (int j = 0; j < city.numRegions(); j++) {
             if (i == j) continue;
-
-            EdgeNode* edge = new EdgeNode;
-            edge->lenght = cost[points[j]];
-
-            cout << i << " " << j << " " << edge->lenght << endl;
-            distMatrix[i][j] = edge->lenght;
-            busFull.addSegment(i, j, edge);
+            distMatrix[i][j] = cost[points[j]];
         }
     }
+}
 
-    //GERAÇÃO BUSLINE
-    int busLine[city.numRegions()];
-    bool visited[city.numRegions()];
-    for(int i = 0; i < city.numRegions(); i++) {busLine[i] = -1; visited[i] = false;}
-    int edgeCount = 0;
+float genBusLines(int numRegions, int busLine[], float** distMatrix){
+    int INF = 2147483647;
+    bool visited[numRegions];
+    for(int i = 0; i < numRegions; i++) {visited[i] = false;}
 
     int v = 0;
     cout << v << " ";
-    busLine[v] = 0;
-    visited[v] = true;
+    busLine[v] = 0; visited[v] = true;
     float totalDist = 0;
+    int edgeCount = 0;
 
-    while(edgeCount != city.numRegions() - 1){
-        int best = -1;
-        int minDist = INF;
+    while(edgeCount != numRegions - 1){
+        int bestV = -1;
+        float minDist = INF;
 
-        for(int other = 0; other < city.numRegions(); other++){
+        for(int other = 0; other < numRegions; other++){
             if(!visited[other] && distMatrix[v][other] < minDist){
                 minDist = distMatrix[v][other];
-                best = other;
+                bestV = other;
             }
         }
-        edgeCount++;
-        busLine[edgeCount] = best;
+        busLine[edgeCount + 1] = bestV; visited[bestV] = true;
         totalDist += minDist;
-        v = best;
-        visited[v] = true;
+        edgeCount++;
+        v = bestV;
         cout << "(" << minDist << ") " << v << " ";
-    }
+    } cout << "(" << distMatrix[v][0] << ")";
     cout << endl << "Total cost: " << totalDist << endl;
-    for(int i = 0; i < city.numRegions() - 1; i++){
-        for(int j = i + 1; j < city.numRegions(); j++){
-            j =
-            int deltaCost = distMatrix[]
+    return totalDist;
+}
+
+void swapPath(int busLine[], int i, int j){
+    i = i + 1;
+    while(i < j){
+        int temp = busLine[i];
+        busLine[i] = busLine[j];
+        busLine[j] = temp;
+        i++; j--;
+    }
+}
+
+float optimizeBusLines(int numRegions, int busLine[], float** distMatrix, float totalDist){
+    bool improved = true;
+    while(improved){
+        improved = false;
+        for(int i = 0; i < numRegions - 1; i++){
+            for(int j = i + 1; j < numRegions; j++){
+                int deltaCost = -distMatrix[busLine[i]][busLine[i + 1]] - distMatrix[busLine[j]][busLine[(j + 1) % numRegions]]
+                               + distMatrix[busLine[i]][busLine[j]] + distMatrix[busLine[i + 1]][busLine[(j + 1) % numRegions]];
+                if (deltaCost < 0){
+                    totalDist += deltaCost;
+                    cout << "swapped " << i << " for " << j << ": " << "[ " << busLine[0];
+                    swapPath(busLine, i, j);
+                    for(int l = 1; l < numRegions; l++){cout << "(" << distMatrix[busLine[l - 1]][busLine[l]] << ") " << busLine[l] << " ";}
+                    cout << "] Total cost: " << totalDist << endl;
+                    improved = true;
+                }
+            }
+        }
+    }
+    return totalDist;
+}
+
+int main(){
+    CityGraph city = cityParser("data/city-1");
+    Graph busFull = Graph(city.numRegions(), 0);
+
+    //ESCOLHA DOS PONTOS QUE DEVE PASSAR NA REGIÃO
+    int points[city.numRegions()];
+    int** path = new int*[city.numRegions()];
+    for (int i = 0; i < city.numRegions(); i++){
+        path[i] = new int[city.numNodes()];
+    }
+
+    for (int i = 0; i < city.numRegions(); i++) {
+        genBusPoints(city, i, points);
+    }
+
+    //GERAÇÃO BUS FULL
+    float** distMatrix = new float*[city.numRegions()];
+    for(int i = 0; i < city.numRegions(); i++){
+        distMatrix[i] = new float[city.numRegions()];
+        for(int j = 0; j < city.numRegions(); j++){
+            distMatrix[i][j] = -1;
         }
     }
 
-    busFull.print();
+    genBusLinesFull(city, points, distMatrix, path);
 
+    for(int i = 0; i < city.numRegions(); i++){
+        for(int j = 0; j < city.numRegions(); j++){
+            cout << distMatrix[i][j] << " ";
+        } cout << endl;
+    }
+
+    //DETERMINAÇÃO DO PRIMEIRO CICLO HAMILTONIANO QUE TEM
+    int busLine[city.numRegions()];
+    for(int i = 0; i < city.numRegions(); i++) {busLine[i] = -1;}
+    float totalDist = genBusLines(city.numRegions(), busLine, distMatrix);
+
+    //OTIMIZAÇÃO DO CICLO O(r^4)
+    totalDist = optimizeBusLines(city.numRegions(), busLine, distMatrix, totalDist);
+
+    //busFull.print();
 
 }
